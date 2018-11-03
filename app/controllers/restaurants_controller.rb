7ad92b5150690c926da750882fd37e0916fbab17
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-03 14:33:46 +0800

    Add a restaurants controller and implement the show method

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,29 @@
+class RestaurantsController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
